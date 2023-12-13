949f7f3cf3ffca5e0d717b9f7f2d5694bca67c9e
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-12-12 16:14:27 -0800

    Fixed mobile menu

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), react()],
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
+  renderers: ["@astrojs/renderer-react"],
 });
--- site/src/components/Header.jsx
+++ site/src/components/Header.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
 const navigation = [
-  { name: 'Resume', href: '/resume' },
-  { name: 'Blog', href: '/blog' },
-  { name: 'Notes & Ramblings', href: '/notes' },
-  { name: 'Projects', href: '/projects' },
-]
+  { name: "Resume", href: "/resume" },
+  { name: "Blog", href: "/blog" },
+  { name: "Notes & Ramblings", href: "/notes" },
+  { name: "Projects", href: "/projects" },
+];
 
 export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
-    <div className="border-b-2 border-yellow-500">
+    <header className="border-b-2 border-yellow-500">
       <nav className="relative container lg:w-2/3 mx-auto py-3 px-6 md:px-0 xxl:w-2/4">
         {/* Flex Container */}
         <div className="flex items-center justify-between w-full">
@@ -15,104 +21,63 @@ export default function Header() {
           <div className="text-left text-2xl">
             <a href="/">Carlos EOS</a>
           </div>
-          <div className="hidden md:flex space-x-4">
-          {/* Menu Items */}
-          {navigation.map((item) => (
-            <a
-              href={item.href}
-              className="hidden md:block p-3 px-6 py-3 hover:bg-primary hover:underline"
-            >
-              {item.name}
-            </a>
-          ))}
-          </div>
-
-          {/* Hamburger Icon */}
+          <div className="flex md:hidden">
           <button
-            id="menu-btn"
-            className="block text-right hamburger md:hidden focus:outline-none"
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
           >
-            <span className="hamburger-top"></span>
-            <span className="hamburger-middle"></span>
-            <span className="hamburger-bottom"></span>
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-
-        {/* Mobile Menu */}
-        <div className="md:hidden">
-          <div
-            id="menu"
-            className="absolute flex-col hidden text-right mt-0 pr-2 space-y-4 bg-white w-24 right-4 border-black-100 border-2"
-          >
-            {navigation.map((item) => (
-              <a href={item.href}>{item.name}</a>
+          <div className="hidden md:flex space-x-4">
+            {/* Menu Items */}
+            {navigation.map((item, index) => (
+              <a
+                key={index}
+                href={item.href}
+                className="hidden md:block p-3 px-6 py-3 hover:bg-primary hover:underline"
+              >
+                {item.name}
+              </a>
             ))}
           </div>
         </div>
-      </nav>
-      <script>
-        {`
-          const btn = document.getElementById("menu-btn");
-          const nav = document.getElementById("menu");
-
-          btn.addEventListener("click", () => {
-            btn.classList.toggle("open");
-            nav.classList.toggle("flex");
-            nav.classList.toggle("hidden");
-          });
-        `}
-      </script>
-
-      <style>
-        {`
-          /* Hamburger Menu */
-          .hamburger {
-            cursor: pointer;
-            width: 24px;
-            height: 24px;
-            transition: all 0.25s;
-            position: relative;
-          }
-
-          .hamburger-top,
-          .hamburger-middle,
-          .hamburger-bottom {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 24px;
-            height: 2px;
-            background: #000;
-            transform: rotate(0);
-            transition: all 0.5s;
-          }
-
-          .hamburger-middle {
-            transform: translateY(7px);
-          }
-
-          .hamburger-bottom {
-            transform: translateY(14px);
-          }
-
-          .open {
-            transform: rotate(90deg);
-            transform: translateY(0px);
-          }
-
-          .open .hamburger-top {
-            transform: rotate(45deg) translateY(6px) translate(6px);
-          }
-
-          .open .hamburger-middle {
-            display: none;
-          }
-
-          .open .hamburger-bottom {
-            transform: rotate(-45deg) translateY(6px) translate(-6px);
-          }
-        `}
-      </style>
-    </div>
+      </nav>    
+      <Dialog as="div" className="md:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="">Carlos E. Ortega</span>
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
   );
 }
